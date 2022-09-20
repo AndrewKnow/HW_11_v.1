40506e1cfb4a97dd2543b1e7faaf63e9087d8df0
AndrewKnow <92088740+AndrewKnow@users.noreply.github.com>
2022-09-20 22:06:21 +0300

    Добавьте файлы проекта.

---
+++ ДЗ_11_v.1/OtusDictionary.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_11_v._1
+{
+    public class OtusDictionary
+    {
+        int sizeArr = 32;
+        DictArr[] dictArr;
+
+        public OtusDictionary()
+        {
+            dictArr = new DictArr[sizeArr];
+        }
+
+        class DictArr
+        {
+            public int Key { get; set; }
+            public string Value { get; set; }
+        }
+
+        public void Add(int key, string value)
+        {
+            try
+            {
+                if (value == null)
+                {
+                    throw new MyExp1();
+                }
+                // Ограничение индексов размером массива
+                int hashKey = key % sizeArr;
+
+                if (dictArr[hashKey] != null)
+                {
+                    // увелчиение массива
+                    dictArr = IncreaseArr(sizeArr);
+                    throw new MyExp2();
+                }
+
+                dictArr[hashKey] = new DictArr() { Key = key, Value = value };
+
+            }
+            catch (MyExp1)
+            {
+                Console.WriteLine("Пустое значение value");
+            }
+            catch (MyExp2)
+            {
+                Console.WriteLine($"Запись с ключом {key % sizeArr} существет, массив увеличен");
+            }
+
+        }
+        public string Get(int key)
+        {
+
+            int hashKey = key % sizeArr;
+
+            try
+            {
+                if (dictArr[hashKey] == null)
+                {
+                    throw new MyExp3();
+                }
+                if (dictArr[hashKey] != null)
+                {
+                    return $"key: {dictArr[hashKey].Key} value: {dictArr[hashKey].Value}";
+                }
+            }
+            catch (MyExp3)
+            {
+                Console.WriteLine($"Ключ {hashKey} не надйен");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine($"Ключ {hashKey} за рамками дипазона");
+            }
+
+            return null;
+        }
+
+        DictArr[] IncreaseArr(int size)
+        {
+
+            sizeArr = size * 2;
+            var increaseArr = new DictArr[sizeArr];
+
+            int hashKey;
+            // передаем данные из dictArr в increaseArr
+            for (int i = 0; i < dictArr.Length; i++)
+            {
+                if (dictArr[i] != null)
+                {
+                    hashKey = dictArr[i].Key % sizeArr;
+                    increaseArr[hashKey] = dictArr[i];
+                }
+            }
+
+            return increaseArr;
+        }
+
+        public string this[int key]
+        {
+            get { return Get(key); }
+            set { Add(key, value); }
+        }
+    }
+
+    internal class MyExp1 : Exception
+    {
+    }
+    internal class MyExp2 : Exception
+    {
+    }
+    internal class MyExp3 : Exception
+    {
+    }
+}
---
+++ ДЗ_11_v.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ДЗ_11_v._1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var dictionary = new OtusDictionary();
+
+            Console.WriteLine("Ввод:");
+
+            dictionary.Add(1, null);
+            dictionary.Add(3, "3 текст");
+            dictionary.Add(2, "2 текст");
+            dictionary.Add(1, "1 текст");
+            dictionary.Add(4, "4 текст");
+            dictionary.Add(5, "5 текст");
+            dictionary.Add(8, "8 текст");
+            dictionary.Add(6, "6 текст");
+            dictionary.Add(7, "7 текст");
+
+
+            Console.WriteLine("Проверка по ключам:");
+
+            Console.WriteLine(dictionary.Get(1));
+            Console.WriteLine(dictionary.Get(2));
+            Console.WriteLine(dictionary.Get(3));
+            Console.WriteLine(dictionary.Get(4));
+            Console.WriteLine(dictionary.Get(33));
+            Console.WriteLine(dictionary.Get(34));
+            Console.WriteLine(dictionary.Get(35));
+            Console.WriteLine(dictionary.Get(200));
+            //dictionary.ArrEnum();
+
+
+
+
+        }
+    }
+}
